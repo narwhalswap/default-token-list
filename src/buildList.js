bcada9c4170f55013a634233e2d1b146db775342
narwhalosaurus <narwhalswap@gmail.com>
2021-09-17 15:07:01 +0700

    feat: add harmony tokens

--- src/buildList.js
+++ src/buildList.js
@@ -1,27 +1,26 @@
-const { version } = require('../package.json');
-const bsc = require('./tokens/bsc.json');
-const bscTestnet = require('./tokens/bsc-testnet.json');
-const matic = require('./tokens/matic.json');
-const moonriver = require('./tokens/moonriver.json');
-const avalanche = require('./tokens/avalanche.json');
-const fantom = require('./tokens/fantom.json');
+const { version } = require("../package.json");
+const bsc = require("./tokens/bsc.json");
+const bscTestnet = require("./tokens/bsc-testnet.json");
+const matic = require("./tokens/matic.json");
+const moonriver = require("./tokens/moonriver.json");
+const avalanche = require("./tokens/avalanche.json");
+const fantom = require("./tokens/fantom.json");
+const harmony = require("./tokens/harmony.json");
 
 module.exports = function buildList() {
-  const parsed = version.split('.');
+  const parsed = version.split(".");
   return {
-    'name': 'Narwhalswap Tokens',
-    'timestamp': (new Date().toISOString()),
-    'version': {
-      'major': +parsed[ 0 ],
-      'minor': +parsed[ 1 ],
-      'patch': +parsed[ 2 ]
+    name: "Narwhalswap Tokens",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
     },
-    'tags': {},
-    'logoURI': 'https://raw.githubusercontent.com/trustwallet/assets/master/dapps/narwhalswap.org.png',
-    'keywords': [
-      'narwhalswap',
-      'default'
-    ],
+    tags: {},
+    logoURI:
+      "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/narwhalswap.org.png",
+    keywords: ["narwhalswap", "default"],
     tokens: [
       ...bsc,
       ...bscTestnet,
@@ -29,6 +28,7 @@ module.exports = function buildList() {
       ...moonriver,
       ...avalanche,
       ...fantom,
+      ...harmony,
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
@@ -36,6 +36,6 @@ module.exports = function buildList() {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
-      })
+      }),
   };
 };
